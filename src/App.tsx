c034cd32d71f755ddcccf4fcca7c5bd63c6cc2fe
TexLuciano <lucianomartinello15@gmail.com>
2023-03-09 17:00:33 -0300

    test loading

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,35 @@
-import React from 'react';
+import React, { FunctionComponentElement, useState } from 'react';
 import styled from 'styled-components';
 import './app.css';
 import Home from './pages/Home';
 import './style/styleinicial.css';
-import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Contato from './pages/Contato';
 
-const Container = styled.div`
-  
-`;
+const Container = styled.div``;
 
 function App() {
+  const spinner = document.getElementById('spinner');
+  const [load, setLoad] = useState(true);
 
+  if (spinner) {
+    setTimeout(() => {
+      spinner.style.display = 'none';
+      setLoad(false);
+    }, 800);
+  }
 
   return (
-    <Router>
-      <Routes>
-        <Route path='/'  element={<Home/>}/>
-        <Route path='/contato' element={<Contato/>} />
-      </Routes>
-    </Router>
+    <>
+      {!load && (
+        <Router>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contato" element={<Contato />} />
+          </Routes>
+        </Router>
+      )}
+    </>
   );
 }
 
